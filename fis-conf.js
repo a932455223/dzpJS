f3af21871592e5da61587c12b42280345adcdbc5
simon.du <a932455223@qq.com>
2017-05-22 10:49:31 +0800

    update package

--- fis-conf.js
+++ fis-conf.js
@@ -1,8 +1,31 @@
-fis.match('*.less', {
+fis.match('**.less', {
   parser: fis.plugin('less-2.x'),
   rExt: '.css'
 })
 
-fis.match('*.less',{
+fis.match('**.less',{
         postprocessor: fis.plugin('less-autoprefix',{browsers:["last 4 versions"]})
+})
+
+fis.match('**.js', {
+  // fis-optimizer-uglify-js 插件进行压缩，已内置
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('**.css', {
+  // fis-optimizer-clean-css 插件进行压缩，已内置
+  optimizer: fis.plugin('clean-css')
+});
+
+fis.match('**.png', {
+  // fis-optimizer-png-compressor 插件进行压缩，已内置
+  optimizer: fis.plugin('png-compressor')
+});
+
+fis.match('**.html', {
+  optimizer: fis.plugin('html-minifier')
+})
+.match('**.{js,less,png,css,jpg,gif}',{
+	useHash:true,
+	url:'/public/dzpJs$0'
 })
